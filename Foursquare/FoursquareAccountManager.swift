4e085a234c097fc5d37e03a31a2272d03ce5d804
Matthew Nanney <mnanney@wayfair.com>
2019-01-12 13:51:05 -0500

    Initial commit of project

---
+++ Foursquare/FoursquareAccountManager.swift
@@ -0,0 +1,18 @@
+//
+//  FoursquareAccountManager.swift
+//  Foursquare
+//
+//  Created by Matthew Nanney on 1/12/19.
+//  Copyright © 2019 Matthew Nanney. All rights reserved.
+//
+
+import Foundation
+
+class FoursquareAccountManager {
+    
+    public var shared = FoursquareAccountManager()
+    
+    private let clientID = "3J5APDTCW15SBTQHEJGDJ4FMU2VFHABNONR35UTOYF1BOANI"
+    private let clientSecret = "F4P1CCVIERX1LJFAZ2AYENBHA3UFZQ5KYMXSK2DSHQQ2P1EO"
+    
+}
